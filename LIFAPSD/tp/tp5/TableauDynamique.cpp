f05a93a6ba04b168595ab2b9682545d1f245aef0
Adam Aysoy MSI <adamoumaradam26@gmail.com>
2023-11-27 01:02:39 +0100

    msi

--- LIFAPSD/tp/tp5/TableauDynamique.cpp
+++ LIFAPSD/tp/tp5/TableauDynamique.cpp
@@ -15,3 +15,13 @@ void TableauDynamique::ajouterElement(ElementTD* e){
    
 };
 */
+// vider le tableau
+void TableauDynamique::vider(){
+    delete [] ad;
+    capacite = 1;
+    taille_utilisee = 0;
+    ad = new ElementTD[capacite];
+}
+// donnes moi le code de la fonction TableauDynamique::ajouterElement
+void TableauDynamique::ajouterElement(ElementTD * e){
+    if(taille_utilisee == capacite ){
--- LIFAPSD/tp/tp5/TableauDynamique.h
+++ LIFAPSD/tp/tp5/TableauDynamique.h
@@ -1,3 +1,4 @@
+
 #ifndef _TAB_DYN
 #define _TAB_DYN
 
@@ -5,9 +6,9 @@
 
 class TableauDynamique {
 public:
-    /* données membres */
+    /* donnï¿½es membres */
     /* =============== */
-    /* (ne sont pas censées être connues par l'utilisateur,
+    /* (ne sont pas censï¿½es ï¿½tre connues par l'utilisateur,
         si on avait pu ne pas les faire figurer dans l'interface, on l'aurait fait)
     */
     unsigned int capacite;
@@ -16,52 +17,52 @@ public:
 
     /* fonctions membres */
     /* ================= */
-    TableauDynamique (); // Constructeur par défaut
-    /* Postcondition : le tableau a 1 emplacement alloué mais vide (taille utilisée nulle) */
+    TableauDynamique (); // Constructeur par dï¿½faut
+    /* Postcondition : le tableau a 1 emplacement allouï¿½ mais vide (taille utilisï¿½e nulle) */
 
     TableauDynamique (const TableauDynamique& t); // Constructeur par copie
-    /* Precondition : le tableau t est initialisé */
-    /* Postcondition : les deux tableaux contiennent des séquences d'ElementTD identiques (copie de t) */
+    /* Precondition : le tableau t est initialisï¿½ */
+    /* Postcondition : les deux tableaux contiennent des sï¿½quences d'ElementTD identiques (copie de t) */
 
     ~TableauDynamique (); // Destructeur
-    /* Postcondition : la mémoire allouée dynamiquement est libérée. */
+    /* Postcondition : la mï¿½moire allouï¿½e dynamiquement est libï¿½rï¿½e. */
 	
 	void vider ();
-    /* Postcondition : la mémoire allouée dynamiquement est libérée.
-	                   le tableau a 1 emplacement alloué mais vide (taille utilisée nulle) */
+    /* Postcondition : la mï¿½moire allouï¿½e dynamiquement est libï¿½rï¿½e.
+	                   le tableau a 1 emplacement allouï¿½ mais vide (taille utilisï¿½e nulle) */
 
     void ajouterElement (ElementTD e);
-    /* Postcondition : l'élément e est ajouté dans le premier emplacement inutilisé du tableau,
-                       la taille est incrémentée de 1. Doublement de la capacité si nécessaire. */
+    /* Postcondition : l'ï¿½lï¿½ment e est ajoutï¿½ dans le premier emplacement inutilisï¿½ du tableau,
+                       la taille est incrï¿½mentï¿½e de 1. Doublement de la capacitï¿½ si nï¿½cessaire. */
 
     ElementTD valeurIemeElement (unsigned int indice) const;
     /* Precondition : 0 <= indice < taille_utilisee */
-    /* Resultat : retourne l'ElementTD à l'indice en paramètre */
+    /* Resultat : retourne l'ElementTD ï¿½ l'indice en paramï¿½tre */
 
     void modifierValeurIemeElement (ElementTD e, unsigned int indice);
     /* Precondition : 0 <= indice < taille_utilisee */
-    /* Postcondition : l'ElementTD à l'indice en paramètre vaut e */
+    /* Postcondition : l'ElementTD ï¿½ l'indice en paramï¿½tre vaut e */
 
     void afficher () const;
-    /* Postcondition : les éléments du tableau sont affichés sur la sortie standard (espacement entre les éléments)
-                       en utilisant la procédure d'affichage de ElementTD */
+    /* Postcondition : les ï¿½lï¿½ments du tableau sont affichï¿½s sur la sortie standard (espacement entre les ï¿½lï¿½ments)
+                       en utilisant la procï¿½dure d'affichage de ElementTD */
 
     void supprimerElement (unsigned int indice);
     /* Precondition : le tableau est non vide, et 0 <= indice < taille_utilisee */
-    /* Postcondition : la taille utilisée du tableau est décrémentée de 1.
-                       si taille_utilisee < capacite/3, alors on divise la capacité par 2. */
+    /* Postcondition : la taille utilisï¿½e du tableau est dï¿½crï¿½mentï¿½e de 1.
+                       si taille_utilisee < capacite/3, alors on divise la capacitï¿½ par 2. */
 
     void insererElement (ElementTD e, unsigned int indice);
     /* Precondition : 0 <= indice < taille_utilisee */
-    /* Postcondition : e est inséré à l'indice en paramètre et la taille utilisée est incrémentée de 1 */
+    /* Postcondition : e est insï¿½rï¿½ ï¿½ l'indice en paramï¿½tre et la taille utilisï¿½e est incrï¿½mentï¿½e de 1 */
 
     void trier ();
-    /* Postcondition : le tableau est trié dans l'ordre croissant des valeurs des éléments */
+    /* Postcondition : le tableau est triï¿½ dans l'ordre croissant des valeurs des ï¿½lï¿½ments */
 
     int rechercherElement (ElementTD e) const;
-    /* Precondition : le tableau est trié dans l'ordre croissant */
-    /* Résultat : indice de l'emplacement qui contient un ElementTD égal à e,
-                  -1 si le tableau ne contient pas d'élément égal à e */
+    /* Precondition : le tableau est triï¿½ dans l'ordre croissant */
+    /* Rï¿½sultat : indice de l'emplacement qui contient un ElementTD ï¿½gal ï¿½ e,
+                  -1 si le tableau ne contient pas d'ï¿½lï¿½ment ï¿½gal ï¿½ e */
 
 };
 
