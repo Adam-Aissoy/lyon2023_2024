4de6a9b9c79fdf39a0712ff16b4cc6721bb0e51e
Adam Aysoy <adamoumaradam26@gmail.com>
2023-11-27 01:08:23 +0100

    dell

--- adam.cpp
+++ adam.cpp
@@ -1,1 +1,10 @@
-test Adam Aysoy
+#include <iostream> // test Adam Aysoy
+// donnes moi une fonction qui retourn la somme de deux int 
+int somme(int x, int y){
+    return x+y;
+}
+// qui est tu  ? 
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
