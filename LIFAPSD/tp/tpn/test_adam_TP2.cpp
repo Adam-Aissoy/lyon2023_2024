6106e014c02e37c83e640748c884e07bbddef5cf
Oumar Adam ADAM <oumar-adam.adam@etu.univ-lyon1.fr>
2024-01-07 10:31:09 +0100

    new send LIFAPCD

--- LIFAPSD/tp/tpn/test_adam_TP2.cpp
+++ LIFAPSD/tp/tpn/test_adam_TP2.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
-
+int variavle(int a, int b){
+    int var = a + b ;
+    return var; 
+}
 
 int main() {
     for(int i = 0; i < 10; i++){
         int randomValue = rand() % 101 - 50;
         std::cout<<randomValue<<std::endl;
     }
+    int v = variavle (100, 100); 
+    std::cout<< v  << std::endl;
     return 0;
 }
