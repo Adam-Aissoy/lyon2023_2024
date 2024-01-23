d7e737ed4ca3392313f17893fead6ce6441adb02
Adam Aysoy <adamoumaradam26@gmail.com>
2024-01-23 18:08:14 +0100

    LIFWEB TP2

---
+++ s6/LIFWEB/tp2/LIFWEB TP2 - DOM_fichiers/tp2_demo_2.js
@@ -0,0 +1,4 @@
+"use strict";
+console.info(`Le script ${document.currentScript.src} s'exécute...`);
+console.debug(document.querySelectorAll("a"));
+console.info(`...le script ${document.currentScript.src} a terminé.`);
---
+++ s6/testTp2.js
@@ -0,0 +1,11 @@
+// Attendez que le DOM soit entièrement chargé
+document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionnez toutes les balises <li> de la page
+    var elementsLi = document.querySelectorAll("li");
+  
+    // Affichez le nombre de balises <li> dans la console
+    console.log("Nombre de balises <li> sur la page : " + elementsLi.length);
+  });
+  
+
+console.log(`je suis sur le console js` );
