c225cd8ca3f817b2c80b8b4240a0c8a3f6f7066f
Adam Aysoy <adamoumaradam26@gmail.com>
2024-01-30 13:02:52 +0100

    LIFWEB_TP2a

--- s6/LIFWEB/tp2/LIFWEB TP2 - DOM_fichiers/tp2.js
+++ s6/LIFWEB/tp2/LIFWEB TP2 - DOM_fichiers/tp2.js
@@ -2,3 +2,44 @@
 console.info("app.js loading...");
 
 console.info("...app.js loaded");
+
+/* Afficher une liste de nom avec prenom dans le body */
+let liste = document.createElement("ul");
+document.body.appendChild(liste);
+let noms = ["Toto", "Titi", "Tata"];
+for (let nom of noms) {
+    let li = document.createElement("li");
+    li.textContent = nom;
+    liste.appendChild(li);
+}
+/*
+new bouton dynamique
+test non reuissit 
+*/
+/*
+
+let raz = document.createElement("button");
+raz.textContent = "RàZ"; 
+raz.id = "raz";
+document.body.appendChild(raz);
+raz.insertAdjacentElement("afterend", document.querySelector("button"));
+raz.addEventListener("click", function() {
+    document.querySelector("input").value = "";
+});
+*/
+document.addEventListener("DOMContentLoaded", function () {
+    var razButton = document.createElement("button");
+    razButton.textContent = "RàZ";
+    var demoButton = document.getElementById("demo-btn");
+    demoButton.insertAdjacentElement('afterend', razButton);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var clickCounter = 0;
+    var demoButton = document.getElementById("demo-btn");
+    var outputCode = document.getElementById("output-code");
+    // gestionnaire pour le compteur de clics sur le bouton Démo
+    demoButton.addEventListener("click", function () {
+      clickCounter++;
+      outputCode.textContent += "Nombre de clics sur Démo : " + clickCounter + "\n";
+    });
+  });
