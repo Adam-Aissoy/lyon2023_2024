183bd914990ac8d5aef630c588a10dc88739783e
Adam Aysoy <adamoumaradam26@gmail.com>
2024-02-21 17:13:24 +0100

    rm cv

--- adam.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> // test Adam Aysoy
-// donnes moi une fonction qui calcule la somme de deux int
-
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
-}
-
