016a159991c00d0906d2759634461a2621078212
Adam Aysoy <adamoumaradam26@gmail.com>
2024-02-21 17:01:47 +0100

    LIFAPOO tp2

--- s6/LIFAPOO/tpNew/tp2/src/CreneauHoraire.java
+++ s6/LIFAPOO/tpNew/tp2/src/CreneauHoraire.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CreneauHoraire {
+public class CreneauHoraire {
+//    Développer une classe CreneauHoraire caractérisée par un numéro de jour, d’ heure et de minute de début, ainsi que par une durée en minutes.
+    public int numJour;
+    public int heureDebut;
+    public int minuteDebut;
+    public int duree;
+    public CreneauHoraire(int numJour, int heureDebut, int minuteDebut, int duree) {
+        this.numJour = numJour;
+        this.heureDebut = heureDebut;
+        this.minuteDebut = minuteDebut;
+        this.duree = duree;
+    }
 }
