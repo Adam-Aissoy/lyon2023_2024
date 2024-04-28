552cea0bf7841d7e15fb6dc40883933cf3e6c61d
Oumar Adam ADAM <oumar-adam.adam@etu.univ-lyon1.fr>
2024-04-29 00:46:10 +0200

    last one revision

--- s4/lifpf/tp/testAdam.ml
+++ s4/lifpf/tp/testAdam.ml
@@ -1,24 +1,9 @@
+(* math_operations.ml *)
 
-(* that's firt comment and first function *)
-(* somme of two int *)
-let somme ( a : int ) ( b : int ) : int = a + b ;; 
-
-(* somme of  int list  *)
-let rec sommeList ( l : int list   ) : int = 
-match l with 
-| [] -> 0
-| tete::queue -> tete + sommeList queue;;
-(*assert sommeList*)
-let l = [1; 2; 3; 4; 5; 6];;
-sommeList l ;
-
-(* test list.map *)
-let rec map (f: 'a -> 'b ) (l: 'a list ) : 'b list =
-  match l with
-  | [] -> []
-  | x :: l' -> (f x )::map f l';;
-(*somme increment *)
-let sommeIncrement (a : int ): int  = a+1;;
-map sommeIncrement l;;
-let cmp x y = x < y;;
-let sup_3 (l:int list): int list = List.filter (cmp 3) l;;
+(* Définition du module MathOperations *)
+module MathOperations = struct
+  let add x y = x + y
+  let subtract x y = x - y
+  let multiply x y = x * y
+  let divide x y = x / y
+end
