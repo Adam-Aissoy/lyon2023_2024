4857fb53bb2dd6cfcf1b6c240b9a2612fcd0fe56
Oumar Adam ADAM <oumar-adam.adam@etu.univ-lyon1.fr>
2024-04-28 23:09:35 +0200

    lifpf revision and reviews cm6 vs cm6New

--- s4/lifpf/tp/testAdam.ml
+++ s4/lifpf/tp/testAdam.ml
@@ -8,3 +8,17 @@ let rec sommeList ( l : int list   ) : int =
 match l with 
 | [] -> 0
 | tete::queue -> tete + sommeList queue;;
+(*assert sommeList*)
+let l = [1; 2; 3; 4; 5; 6];;
+sommeList l ;
+
+(* test list.map *)
+let rec map (f: 'a -> 'b ) (l: 'a list ) : 'b list =
+  match l with
+  | [] -> []
+  | x :: l' -> (f x )::map f l';;
+(*somme increment *)
+let sommeIncrement (a : int ): int  = a+1;;
+map sommeIncrement l;;
+let cmp x y = x < y;;
+let sup_3 (l:int list): int list = List.filter (cmp 3) l;;
