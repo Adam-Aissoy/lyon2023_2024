123671c4c7347e85980bef927a5dee8d7ca3b919
Adam Aysoy <adamoumaradam26@gmail.com>
2024-05-13 06:06:46 +0200

    last on

--- s4/LIFPROLOG/test.pl
+++ s4/LIFPROLOG/test.pl
@@ -1,110 +1,12 @@
-/*
-Nom: Adam
-Prenom : Oumar Adam
-Numero : p2110686
-*/
-/* Tp resolution de probleme de satisfaction de contraintes */
-/*1) 
-*/
-domaines(L) :- 
-    L = [[1,[rouge,vert,bleu,jaune]],
-        [2,[rouge,vert,bleu,jaune]],
-        [3,[rouge,vert,bleu,jaune]],
-        [4,[rouge,vert,bleu,jaune]],
-        [5,[rouge,vert,bleu,jaune]],
-        [6,[rouge,vert,bleu,jaune]],
-        [7,[rouge,vert,bleu,jaune]]].
-/* 
-test
-?- domaines(L).
-L = [[1, [rouge, vert, bleu, jaune]], [2, [rouge, vert, bleu, jaune]], [3, [rouge, vert, bleu, jaune]], [4, [rouge, vert, bleu|...]], [5, [rouge, vert|...]], [6, [rouge|...]], [7, [...|...]]].
-*/
-voisins(X,Y) :- 
-    L = [[1,2],[1,3],[1,7],[2,3],[2,4],[2,5],[3,4],[3,5],[3,6],[3,7],[4,5],[5,6],[6,7]],
-    member([X,Y],L);member([Y,X],L).
-   
+% CC AUTOMNE 2022 PRO-LOG
 
-/*test 
+/* Définir un predicat porduitScalaire(L1, L2, L3) renvoie la somme de la multiplication des elements : L1 = (x1, x2) L2 = (y1, y2), L3 = X1*Y1 + X2*Y2 */
 
-?- voisins(X,Y).
-X = 1,
-Y = 2 ;
-X = 1,
-Y = 3 ;
-*/
+produitScalaire([],[],0).
+produitScalaire([X|L1],[Y|L2], R):- produitScalaire(L1,L2,N), R is N + X * Y.
 
-consistants([X1,V1],[X2,V2]) :- 
-    voisins(X1,X2) -> V1 \= V2;true.
-consistants([X1,V1],[X2,V2]) :- 
-    not(voisins(X1,X2)) -> V1 = V2;true.
+/* On supprime N fois l element X de la liste L, on renvoie la liste a jour danns R */
 
-/*
-[5]  ?- consistants([1,rouge],[2,vert]).
-true ;
-true .
-[13]  ?- consistants([1,rouge],[2,rouge]).
-false.
-
-*/
-/*2)   genererEtTeste(Solution) 
-?- genererEtTeste(Solution).
-Solution = [[1, rouge], [2, jaune], [3, vert], [4, rouge], [5, bleu], [6, rouge], [7, bleu]] ;
-*/
-
-
-
-/*generer et tester */
-
-tester(X,Y) :- 
-    not((member(A,X),member(B,Y),consistants(A,B))).
-tester([]).
-tester([X|L]) :- 
-    tester(X,L),tester(L).
-
-genererEtTeste(Solution) :- 
-    domaines(L),
-    generer(L,Solution),
-    tester(Solution).
-generer([],[]).
-generer([[X,D]|L],[[X,V]|S]) :- 
-    member(V,D),
-    generer(L,S).
-
-
-/*test
-
-?- generer(L,S).
-L = S, S = [] ;
-L = [[_A, [_B|_]]],
-S = [[_A, _B]]
-*/
-
-/*
-
-[6]  ?- genererEtTeste(Solution).
-Solution = [[1, rouge], [2, rouge], [3, rouge], [4, rouge], [5, rouge], [6, rouge], [7, rouge]]
-% Break level */
-
-
-  
-/*3) */
-affectationPartielle(Solution) :- 
-    domaines(L),
-    affectationPartielle(L,[],Solution),
-    tester(Solution).
-affectationPartielle([],Solution,Solution).
-affectationPartielle([[X,D]|L],Solution,Res) :- 
-    member(V,D),
-    affectationPartielle(L,[[X,V]|Solution],Res).
-
-    /*
-        affectationPartielle(Solution) .
-Solution = [[7, rouge], [6, rouge], [5, rouge], [4, rouge], [3, rouge], [2, rouge], [1, rouge]] ;
-    */
-
-
-
-
-
-
-/**/
+mystere(0,_,L,L):- !.
+mystere(N,X,[X|L],R):- !,Nm1 is N - 1, mystere(Nm1,X,L,R).
+mystere(N,X,[Y|L],[Y|R]):- mystere(N,X,L,R).
--- s4/LIFSE/tp/test.cpp
+++ s4/LIFSE/tp/test.cpp
@@ -1,58 +1,20 @@
-/*r ← fork()
-si r > 0 alors // bloc exécuté par le processus père
-  pour i de 1 à 26 faire
-    sleep(1)
-    print(i)
-    kill(r, SIGCONT)
-    pause()
-    // le père attend que son fils se termine
-    return 0
-
-sinon // bloc exécuté par le processus fils
-  pour c de ’a’ à ’z’ faire
-  pause()
-  print(c)
-  kill(getppid(), SIGCONT)
-  
-return 0*/
 #include <iostream>
 #include <unistd.h>
-#include <signal.h>
-#include <cstring>
 #include <sys/wait.h>
+#include <cstdlib>
+using namespace std;
 
-void handler(int sig) {
-    static int cpt = 0;
-    if (sig == SIGUSR1) {
-        std::cout << "Signal " << sig << " reçu" << std::endl;
-        cpt++;
-        if (cpt == 5) {
-            std::cout << "fin du programme" << std::endl;
-            exit(0);
-        }
+
+int main(int argc, char *argv[]) {
+    int a = 2;
+    int ret = fork();
+    if (ret == 0) { //   processus fils
+        sleep(1);
+        cout << a << endl;
     }
-}
-
-int main(void) {
-    struct sigaction action;
-    memset(&action, 0, sizeof(action));
-    action.sa_handler = handler;
-    sigaction(SIGUSR1, &action, NULL);
-
-    pid_t r = fork();
-    if (r > 0) {
-        for (int i = 1; i <= 26; i++) {
-            sleep(1);
-            std::cout << i << std::endl;
-            kill(r, SIGCONT);
-            pause();
-        }
-    } else {
-        for (char c = 'a'; c <= 'z'; c++) {
-            pause();
-            std::cout << c << std::endl;
-            kill(getppid(), SIGCONT);
-        }
+    else { // processus père
+        a = 4;
+        cout << a << endl;
     }
-    return 0;
+    return EXIT_SUCCESS ;
 }
